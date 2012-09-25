23bff8617868328b226cdd4d066a5b3e957470c6
gremlin <gremlin@gremlin-mint.(none)>
2012-09-25 23:13:06 +0300

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManager::Application.config.secret_token = '5bcb31b2418a73652f7e152eb19c88be169caee22351bd070ea0657eb809178da3b92d1a1ef3687ac21973113aff6a0fa4a0a2512033182f1e570ed17816ac88'
