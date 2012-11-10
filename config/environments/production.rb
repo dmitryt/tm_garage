5315639a62c48cdadaaf26b59b36928ec156101b
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2012-11-11 01:52:28 +0200

    resolving problem with pictures on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,6 @@ TaskManager::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = nil
-
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
