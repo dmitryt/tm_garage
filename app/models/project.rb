b0eaa92550f4f017a6e6207a788c06441e7ea9ad
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2012-11-12 00:11:10 +0200

    removed erb extensions in styles

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
   belongs_to :user
   has_many :tasks, :dependent => :delete_all
 
-  validates :title, :presence => true, :uniqueness => {:scope => :user_id}, :length => {:maximum => 3}
+  validates :title, :presence => true, :uniqueness => {:scope => :user_id}, :length => {:maximum => 50}
 end
